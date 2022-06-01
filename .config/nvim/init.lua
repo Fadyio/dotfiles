06eac70300da2659b19953881d9a80c0e6086fd1
fady0 <fadinagh0@gmail.com>
2022-06-01 21:52:08 +0200

    cleanup some stuff

---
+++ .config/nvim/init.lua
@@ -0,0 +1,3 @@
+require "f0dy.options"
+require "f0dy.keymaps"
+require "f0dy.plugins"
---
+++ .config/nvim/lua/f0dy/keymaps.lua
@@ -0,0 +1,54 @@
+--[[
+        http://github.com/fady0
+---------------------------------------------------------------------
+                                 keymaps
+---------------------------------------------------------------------
+--]]
+
+local opts = { noremap = true, silent = true }
+
+local term_opts = { silent = true }
+
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+--Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+-- Normal --
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- 
+keymap("n", "<leader>e", ":Lex 20<cr>", opts)
+
+
+-- Resize with C + arrows 
+keymap("n", "<C-Up>", ":resize +2<CR>", opts)
+keymap("n", "<C-Down>", ":resize -2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- Navigate buffers with Capital L H
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- Visual --
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+keymap("v", "p", '"_dP', opts)
+
