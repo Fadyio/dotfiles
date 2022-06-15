9af3bde38550ba14bdcc1d6669da52d271cf8cad
fady0 <fadinagh0@gmail.com>
2022-06-15 19:41:40 +0200

    move my config from vim script to lua

--- .config/nvim/lua/config/statusline.lua
+++ .config/nvim/lua/config/statusline.lua
@@ -13,14 +13,11 @@ require("lualine").setup({
 		lualine_a = { "mode" },
 		lualine_b = { "branch", "diff", "diagnostics" },
 		--	lualine_c = { "filename" },
-		 { separator },
-        { lsp_client, icon = " ", color = { fg = colors.violet, gui = "bold" } },
-        { lsp_progress },
 		lualine_x = { "encoding", "fileformat", "filetype" },
 		lualine_y = { "progress" },
 		lualine_z = { "location" },
 	},
-	
+
 	inactive_sections = {
 		lualine_a = {},
 		lualine_b = {},
