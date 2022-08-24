a7b51aa4284138fdbf58b259eae2d46083e6488f
Fady Nagh <fady@fadyio.com>
2022-08-24 16:43:03 +0200

    change the play speed of mvp

--- .screenlayout/book.sh
+++ .screenlayout/book.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP1 --off --output HDMI1 --mode 1280x720 --pos 0x396 --rotate normal --output VGA1 --mode 1600x900 --pos 1280x0 --rotate left --output VIRTUAL1 --off
+xrandr --output DP1 --off --output HDMI1 --mode 1280x720 --pos 900x213 --rotate normal --output VGA1 --mode 1600x900 --pos 0x0 --rotate right --output VIRTUAL1 --off
