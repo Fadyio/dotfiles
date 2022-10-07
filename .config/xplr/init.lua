784e6c49a33b0cb740d7e6ea873db72d3fac232d
Fady Nagh <fady@fadyio.com>
2022-10-07 08:06:24 +0200

    add xplr config

--- .config/xplr/init.lua
+++ .config/xplr/init.lua
@@ -1,1 +1,65 @@
 version = "0.19.3"
+
+local function stat(node)
+  return node.mime_essence
+end
+
+local function read(path, lines)
+  local out = ""
+  local p = io.open(path)
+
+  if p == nil then
+    return stat(path)
+  end
+
+  local i = 0
+  for line in p:lines() do
+    out = out .. line .. "\n"
+    if i == lines then
+      break
+    end
+    i = i + 1
+  end
+  p:close()
+
+  return out
+end
+
+xplr.config.layouts.builtin.default = {
+  Horizontal = {
+    config = {
+      constraints = {
+        { Percentage = 60 },
+        { Percentage = 40 },
+      },
+    },
+    splits = {
+      "Table",
+      {
+        CustomContent = {
+          title = "preview",
+          body = { DynamicParagraph = { render = "custom.preview_pane.render" } },
+        },
+      },
+    },
+  },
+}
+
+xplr.fn.custom.preview_pane = {}
+xplr.fn.custom.preview_pane.render = function(ctx)
+  local n = ctx.app.focused_node
+
+  if n and n.canonical then
+    n = n.canonical
+  end
+
+  if n then
+    if n.is_file then
+      return read(n.absolute_path, ctx.layout_size.height)
+    else
+      return stat(n)
+    end
+  else
+    return ""
+  end
+end
