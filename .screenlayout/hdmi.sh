93a849ad2635ed6ef48559c9d3e2a9744e34d69d
Fady Nagh <fady@fadyio.com>
2022-12-27 20:06:20 +0200

    add some mpv scripts

--- .screenlayout/hdmi.sh
+++ .screenlayout/hdmi.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP1 --off --output HDMI1 --mode 1280x720 --pos 1600x90 --rotate normal --output VGA1 --mode 1600x900 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+xrandr --output DP1 --off --output HDMI1 --mode 1360x768 --pos 1600x90 --rotate normal --output VGA1 --mode 1600x900 --pos 0x0 --rotate normal --output VIRTUAL1 --off
