18e6b1e82919363ce39dea5c5852aacb1dc0eb36
Fady <Fady@Fadyio.com>
2023-01-30 16:46:04 +0200

    add gpg sign to git config

--- .screenlayout/hdmi.sh
+++ .screenlayout/hdmi.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP1 --off --output HDMI1 --mode 1360x768 --pos 0x66 --rotate normal --output VGA1 --mode 1600x900 --pos 1360x0 --rotate normal --output VIRTUAL1 --off
+xrandr --output DP1 --off --output HDMI1 --mode 1360x768 --pos 1600x66 --rotate normal --output VGA1 --mode 1600x900 --pos 0x0 --rotate normal --output VIRTUAL1 --off
