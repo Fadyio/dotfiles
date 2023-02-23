ee0390059f7becbbc3e3c813156a2bac3528983b
Fady <Fady@Fadyio.com>
2023-02-23 21:23:41 +0200

    refactor the repo and add macOS dotfiles

---
+++ .config/sway/autostart.sh
@@ -0,0 +1,14 @@
+# SwayWM
+flameshot &
+
+# i3WM
+# xcape -e 'Control_L=Escape' &
+# setxkbmap -option ctrl:nocaps &
+# ulauncher &
+# megasync &
+# dropbox &
+# copyq &
+# flameshot &
+# unclutter &
+# picom &
+# sh ~/.dotfiles/scripts/feh-wall.sh &
